450ba804d09954cc78803eaa14f51fa7e523a09b
yukirop <107736439+yukirop@users.noreply.github.com>
2022-08-17 15:32:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,57 +3,56 @@ var windowSm = 768;
 if (windowWidth <= windowSm) {
 //横幅768px以下（スマホ）に適用させるJavaScriptを記述
 
-const htu_steps = document.querySelectorAll(".step_animation")
-
-htu_steps.forEach((target, i) => {
-    // if (i !== 0) {
-    //     gsap.set(target, {
-    //         opacity: 0,
-    //     })
-    // }
-    gsap.set(target, {
-        opacity: 0,
-    })
-
-    ScrollTrigger.create({
-        start: "center center",
-        end: "+=900",
-        trigger: target,
-        // markers: true,
-        pin: true,
-        // onEnter: () => {
-        //     gsap.to(target, { opacity: 1, duration: 0.8})
-        // },
-        // onEnterBack: () => {
-        //     gsap.to(target, { opacity: 1, duration: 0.8})
-        // },
-        // onLeave: () => {
-        //     // gsap.to(target, { opacity: 0, duration: 0.8})
-        // },
-        // onLeaveBack: () => {
-        //     gsap.to(target, { opacity: 0, duration: 0.8})
-        // },
-    })
-})
-
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-        gsap.to(entry.target, { opacity: 1, duration: 0})
-    } else {
-      gsap.to(entry.target, { opacity: 0, duration: 0})
-    }
-  });
-}, {
-  rootMargin: "-21% 0px -23% 0px",
-  threshold: 1,
-});
-
-htu_steps.forEach(item => {
-  observer.observe(item);
-});
+} else{
     
-}else{
-    break;
+    const htu_steps = document.querySelectorAll(".step_animation")
+
+    htu_steps.forEach((target, i) => {
+        // if (i !== 0) {
+        //     gsap.set(target, {
+        //         opacity: 0,
+        //     })
+        // }
+        gsap.set(target, {
+            opacity: 0,
+        })
+
+        ScrollTrigger.create({
+            start: "center center",
+            end: "+=900",
+            trigger: target,
+            // markers: true,
+            pin: true,
+            // onEnter: () => {
+            //     gsap.to(target, { opacity: 1, duration: 0.8})
+            // },
+            // onEnterBack: () => {
+            //     gsap.to(target, { opacity: 1, duration: 0.8})
+            // },
+            // onLeave: () => {
+            //     // gsap.to(target, { opacity: 0, duration: 0.8})
+            // },
+            // onLeaveBack: () => {
+            //     gsap.to(target, { opacity: 0, duration: 0.8})
+            // },
+        })
+    })
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            gsap.to(entry.target, { opacity: 1, duration: 0})
+        } else {
+          gsap.to(entry.target, { opacity: 0, duration: 0})
+        }
+      });
+    }, {
+      rootMargin: "-21% 0px -23% 0px",
+      threshold: 1,
+    });
+
+    htu_steps.forEach(item => {
+      observer.observe(item);
+    });
 }
 
